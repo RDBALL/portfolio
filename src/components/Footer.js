48df4ff7dbd98b522f7cd3988781a6132d570fde
Robert Ball <bobball89@gmail.com>
2022-08-19 14:44:24 -0700

    updated projects, removed second language

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,14 +24,14 @@ class Footer extends Component {
                 onClick={() =>
                   this.props.applyPickedLanguage(
                     window.$primaryLanguage,
-                    window.$secondaryLanguageIconId
+
                   )
                 }
                 style={{ display: "inline" }}
               >
                 <span
                   className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
+
                   data-inline="false"
                   id={window.$primaryLanguageIconId}
                 ></span>
@@ -39,7 +39,7 @@ class Footer extends Component {
               <div 
                 onClick={() =>
                   this.props.applyPickedLanguage(
-                    window.$secondaryLanguage,
+
                     window.$primaryLanguageIconId
                   )
                 }
@@ -47,7 +47,7 @@ class Footer extends Component {
               >
                 <span
                   className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
+
                   data-inline="false"
                   id={window.$secondaryLanguageIconId}
                 ></span>
